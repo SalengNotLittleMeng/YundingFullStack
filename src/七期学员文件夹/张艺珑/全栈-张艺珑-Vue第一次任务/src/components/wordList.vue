69de160a378ecd14e5b2b9e899b1ce212d5de38b
zyl <1282432710@qq.com>
2023-04-05 09:45:05 +0800

    全栈-张艺珑-Vue第一次任务

---
+++ src/七期学员文件夹/张艺珑/全栈-张艺珑-Vue第一次任务/src/components/wordList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { inject } from 'vue';
+let { words } = inject('words')
+function getClass() {
+    if (words.value.length > 5) {
+        return 'over'
+    }
+}
+</script>
+
+<template>
+    <ul :class="getClass()">单词列表：
+        <li v-for="word in words">{{ word }}</li>
+    </ul>
+</template>
+
+<style scoped>
+    .over {
+        color: red;
+    }
+</style>
