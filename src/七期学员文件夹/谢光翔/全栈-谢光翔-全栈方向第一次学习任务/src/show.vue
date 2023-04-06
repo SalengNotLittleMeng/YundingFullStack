3cd97c9b17ae32d1c3678cb91026d0e9bcac0368
谢光翔 <xieguangxiang2020@outlook.com>
2023-04-06 16:40:03 +0800

    全栈-谢光翔-全栈方向第一次学习任务

---
+++ src/七期学员文件夹/谢光翔/全栈-谢光翔-全栈方向第一次学习任务/src/show.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {computed} from 'vue';
+    const props=defineProps(["showLists"]);
+    const is_much=computed(()=>{
+        return props.showLists.length>5
+    });
+</script>
+<template>
+    <div :class="{ul_color:is_much}">
+        <p v-if="is_much">鳖甲辣！单词数量太多了</p>
+        <p>单词列表：</p>
+        <ul>
+            <li v-for="key in props.showLists">{{ key.word }}</li>
+        </ul>
+    </div>
+</template>
+<style>
+.ul_color{
+    color: red;
+}
+</style>
