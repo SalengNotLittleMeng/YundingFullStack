804eb96f90018a5934ab14cc6f6b25fd2c0b9f96
marongzhen <2712268578@qq.com>
2023-04-15 18:50:33 +0800

    全栈-马荣臻-Vue第二次任务

---
+++ src/七期学员文件夹/马荣臻/全栈-马荣臻-Vue第二次任务/src/assets/data.js
@@ -0,0 +1,40 @@
+export default {
+        books: [
+        {
+            id: 1,
+            name: '《算法导论》',
+            date: '2006 - 9 ',
+            price: 85.00,
+            count: 1
+        },
+        {
+            id: 2,
+            name: '《UNIX编程艺术》',
+            date: ' 2006-2',
+            price: 59.00,
+            count: 1
+        },
+        {
+            id: 3,
+            name: '《编程珠玑》',
+            date: ' 2008-10',
+            price: 39.00,
+            count: 3
+        },
+        {
+            id: 4,
+            name:
+                '《代码大全》',
+            date: ' 2006-3',
+            price: 128.00,
+            count: 1
+        },
+        {
+            id: 5,
+            name: '《你不知道JavaScript》',
+            date: ' 2014-8',
+            price: 88.00,
+            count: 1
+        },
+    ]
+}
