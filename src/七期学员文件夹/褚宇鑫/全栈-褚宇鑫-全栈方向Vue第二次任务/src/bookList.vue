5828e3268eea31578fd70d7808410b9a15a27934
Money <2932978634@qq.com>
2023-04-17 16:23:28 +0800

    提交了李静怡与褚宇鑫的vue第二次作业 提交了不使用脚手架实现的作业案例代码

---
+++ src/七期学员文件夹/褚宇鑫/全栈-褚宇鑫-全栈方向Vue第二次任务/src/bookList.vue
@@ -0,0 +1,67 @@
+<template>
+    <!-- <div class="book" :class="{ 'selected': isSelected }" @click="selectBook"> -->
+      <tr class="book" :class="{ 'selected': isSelected }" @click="selectBook">
+          <td>{{ title }}</td>
+          <td>{{ author }}</td>
+          <td>{{ publishedDate }}</td>
+          <td>{{ price }}</td>
+          <td>
+            <button :disabled="count <= 1" @click="delCount(index)">-</button>
+            {{count }}
+            <button @click="addCount(index)">+</button>
+          </td>
+          <td>
+            <!-- <button @click="selectBook(index)">选中</button> -->
+            <button @click.stop="removeBook(index)">删除</button>
+          </td>
+        </tr>
+    <!-- </div> -->
+  </template>
+  
+  <script>
+  export default {
+    props: ['title', 'author', 'publishedDate', 'price', 'count' ,'isSelected'],
+    methods: {
+      selectBook() {
+        this.$emit('book-selected');
+      },
+      removeBook() {
+        this.$emit('book-removed');
+      },
+      addCount(){
+          this.$emit('count-add')
+      },
+      delCount(){
+        this.$emit('count-del')
+      },
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .book {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin-bottom: 10px;
+    cursor: pointer;
+  }
+  
+  table.books {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+table.books th,
+table.books td {
+  border: 1px solid #ccc;
+  padding: 10px;
+  text-align: center;
+}
+
+table.books th {
+  background-color: #eee;
+}
+  .selected {
+    background-color: lightblue;
+  }
+  </style>
