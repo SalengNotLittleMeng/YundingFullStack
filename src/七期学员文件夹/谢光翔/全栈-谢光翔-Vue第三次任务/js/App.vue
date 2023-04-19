846c7b499c2d7d8ff10e86d86a6211bf5de00d33
谢光翔 <xieguangxiang2020@outlook.com>
2023-04-19 20:32:28 +0800

    全栈-谢光翔-Vue第三次任务

---
+++ src/七期学员文件夹/谢光翔/全栈-谢光翔-Vue第三次任务/js/App.vue
@@ -0,0 +1,100 @@
+<script setup>
+    import artist from '../data/artist';
+    import artList from './myList.vue';
+    import newSong from '../data/newSong';
+    import hotSong from '../data/hotSong';
+    import originSong from '../data/originSong';
+</script>
+
+<template>
+  <h3>热门歌手</h3>
+  <hr>
+  <artList :list-content="artist" :ul-show="true" :ul-display="true" v-slot="slotProps">
+    <div id="singer">
+        <img :src="slotProps.content.picUrl">
+        <span>{{ slotProps.content.name }}</span>
+    </div>
+  </artList>
+  <hr>
+  <h3>榜单</h3>
+  <div id="songs">
+    <div class="song_head_wrapper">
+      <artList :list-content="newSong">
+      <template v-slot:top="topProp">
+        <div class="songHead">
+            <img :src="topProp.content.coverImgUrl">
+            <span>{{ topProp.content.name }}</span>
+            <p>{{ topProp.content.description }}</p>
+        </div>
+      </template> 
+    </artList>
+    <artList :list-content="newSong.tracks" :ul-show="true">
+      <template v-slot:default="slotProps">
+        {{ slotProps.index+1 }}-{{ slotProps.content.name }}
+      </template> 
+    </artList>
+    </div>
+    <div class="song_head_wrapper">
+      <artList :list-content="hotSong">
+      <template v-slot:top="topProp">
+        <div class="songHead">
+            <img :src="topProp.content.coverImgUrl">
+            <span>{{ topProp.content.name }}</span>
+            <p>{{ topProp.content.description }}</p>
+        </div>
+      </template> 
+    </artList>
+    <artList :list-content="hotSong.tracks" :ul-show="true">
+      <template v-slot:default="slotProps">
+        {{ slotProps.index+1 }}-{{ slotProps.content.name }}
+      </template> 
+    </artList>
+    </div>
+    <div class="song_head_wrapper">
+      <artList :list-content="originSong">
+      <template v-slot:top="topProp">
+        <div class="songHead">
+            <img :src="topProp.content.coverImgUrl">
+            <span>{{ topProp.content.name }}</span>
+            <p>{{ topProp.content.description }}</p>
+        </div>
+      </template> 
+    </artList>
+    <artList :list-content="originSong.tracks" :ul-show="true">
+      <template v-slot:default="slotProps">
+        {{ slotProps.index+1 }}-{{ slotProps.content.name }}
+      </template> 
+    </artList>
+    </div>
+  </div>
+</template>
+
+<style>
+    #singer img{
+        width:60px;
+        height: 60px;
+        display: block;
+    }
+    #songs{
+      width: 100%;
+      display: flex;
+      justify-content: space-around;
+    }
+    .song_head_wrapper{
+      width: 33.3%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+    }
+    .songHead {
+      width: 95%;
+    }
+    .songHead img{
+      width: 80px;
+      float: right;
+    }
+    .songHead span{
+      font-weight: bold;
+      font-size: 20px;
+    }
+</style>
---
+++ src/七期学员文件夹/谢光翔/全栈-谢光翔-Vue第三次任务/js/myList.vue
@@ -0,0 +1,31 @@
+<script setup>
+    const props=defineProps(["listContent","ulShow","ulDisplay"]);
+</script>
+
+<template>
+    <slot name="top" :content="props.listContent"></slot>
+    <ul class="artist_li" :class="{artist_li_display:props.ulDisplay}" v-if="props.ulShow">
+        <li v-for="artists,index in props.listContent">
+            <slot :content="artists" :index="index"></slot>
+        </li>
+  </ul>
+</template>
+
+<style>
+    .artist_li{
+        width: 100%;
+        height: auto;
+        list-style: none;
+        padding: 0;
+    }
+    .artist_li_display{
+        display: flex;
+        justify-content: space-around;
+        text-align: center;
+    }
+    .artist_li li{
+        font-weight: bold;
+        line-height: 20px;
+        margin: 0 5px;
+    }
+</style>
