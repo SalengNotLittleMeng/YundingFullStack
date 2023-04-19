5fd0c2e606f22512ba578578289365cbd414cf92
marongzhen <2712268578@qq.com>
2023-04-19 11:45:41 +0800

    全栈-马荣臻-Vue第三次任务

---
+++ src/七期学员文件夹/马荣臻/全栈-马荣臻-Vue第三次任务/网易云/src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <list title="热门歌手">
+      <template #content>
+        <div class="list">
+          <div v-for="item in artist" :key="item.name">
+            <img :src="item.picUrl" alt="" />
+            <p>{{ item.name }}</p>
+          </div>
+        </div>
+      </template>
+    </list>
+    <list title="榜单">
+      <template #content>
+        <div class="list">
+          <list title="新歌榜">
+            <template #titleContent>
+              <div class="description">
+                <p>{{ hotSong.description }}</p>
+                <img :src="hotSong.coverImgUrl" alt="" class="cover" />
+              </div>
+            </template>
+            <template #content>
+              <div v-for="(item, index) in hotSong.tracks" :key="item.name">
+                <p>{{ index + 1 }} - {{ item.name }}</p>
+              </div>
+            </template>
+          </list>
+          <list title="热歌榜">
+            <template #titleContent>
+              <div class="description">
+                <p>{{ newSong.description }}</p>
+                <img :src="newSong.coverImgUrl" alt="" class="cover" />
+              </div>
+            </template>
+            <template #content>
+              <div v-for="(item, index) in newSong.tracks" :key="item.name">
+                <p>{{ index + 1 }} - {{ item.name }}</p>
+              </div>
+            </template>
+          </list>
+          <list title="原创榜">
+            <template #titleContent>
+              <div class="description">
+                <p>{{ originSong.description }}</p>
+                <img :src="originSong.coverImgUrl" alt="" class="cover" />
+              </div>
+            </template>
+            <template #content>
+              <div v-for="(item, index) in originSong.tracks" :key="item.name">
+                <p>{{ index + 1 }} - {{ item.name }}</p>
+              </div>
+            </template>
+          </list>
+        </div>
+      </template>
+    </list>
+  </div>
+</template>
+
+<script>
+import list from "./components/list";
+import artist from "./assets/data/artist.js";
+import hotSong from "./assets/data/hotSong.js";
+import newSong from "./assets/data/newSong.js";
+import originSong from "./assets/data/originSong.js";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      artist,
+      hotSong,
+      newSong,
+      originSong,
+    };
+  },
+  components: {
+    list,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 10px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  text-align: left;
+}
+
+img {
+  height: 30px;
+  width: 30px;
+}
+
+p {
+  font-size: 10px;
+}
+
+.list {
+  display: flex;
+  margin: 10px;
+}
+
+.list * {
+  flex: 1;
+}
+
+.description {
+  display: flex;
+  margin-bottom: 5px;
+}
+
+.cover {
+  display: inline;
+  width: 100px;
+  height: 100px;
+  flex: 0;
+}
+</style>
---
+++ src/七期学员文件夹/马荣臻/全栈-马荣臻-Vue第三次任务/网易云/src/components/list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h2>{{ title }}</h2>
+    <div class="title">
+      <slot name="titleContent"></slot>
+    </div>
+    <slot name="content" class="slot">
+      <p class="defaultContent">默认内容</p>
+    </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "msgList",
+  props: {
+    title: {
+      type: String,
+      default: "默认标题",
+    },
+  },
+  data() {},
+};
+</script>
+
+<style scoped>
+* {
+  text-align: left;
+}
+
+div {
+  border-bottom: 1px solid #000;
+}
+
+.title {
+  border-bottom: 3px solid #000;
+}
+</style>
