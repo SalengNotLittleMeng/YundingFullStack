5d206a3a47531992439b77ccef1d33362fa4748c
Xirealm <1401443038@qq.com>
2023-04-19 20:30:43 +0800

    全栈-米英杰-Vue第三次任务
    全栈-米英杰-Vue第三次任务

---
+++ src/七期学员文件夹/米英杰/全栈-米英杰-Vue第三次任务/Cloud Music/src/components/BaseLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <header>
+            <slot name="header"></slot>
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+    </div>
+</template>
+
+<style>
+    .container{
+        margin: 10px;
+    }
+
+    header{
+        border-bottom: solid 2px black;
+    }
+
+    main{
+        border-bottom: solid 1px black;
+        padding-bottom: 5px;
+    }
+</style>
