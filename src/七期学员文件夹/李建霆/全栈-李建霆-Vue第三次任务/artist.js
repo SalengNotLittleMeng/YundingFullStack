85a25f744c90ba3ad1434782a2e56d991d9a8c8e
angler <2926686459@qq.com>
2023-04-20 20:53:27 +0800

    全栈-李建霆-Vue第三次作业提交

---
+++ src/七期学员文件夹/李建霆/全栈-李建霆-Vue第三次任务/artist.js
@@ -0,0 +1,73 @@
+export default
+  [
+  {
+    name: "林俊杰",
+    picUrl:
+      "http://p1.music.126.net/78q0jUUJ0h08GxAs2G-tCA==/109951168529051968.jpg",
+    altUrl:
+      "http://p1.music.126.net/r6W-zCnV-aduVn_PLZYuYg==/109951168529049969.jpg",
+  },
+  {
+    name: "薛之谦",
+    picUrl:
+      "http://p1.music.126.net/LCWqYYKoCEZKuAC3S3lIeg==/109951165034938865.jpg",
+    altUrl:
+      "http://p1.music.126.net/1tSJODTpcbZvNTCdsn4RYA==/109951165034950656.jpg",
+  },
+  {
+    name: "陈奕迅",
+    picUrl:
+      "http://p1.music.126.net/w_vuv9hBWq2hlJxJcmJrjg==/109951166115915081.jpg",
+    altUrl:
+      "http://p1.music.126.net/rYYhHXZHwCfizE0N46F37Q==/109951166115911716.jpg",
+  },
+  {
+    name: "邓紫棋",
+    picUrl:
+      "http://p1.music.126.net/fq1O8ZRT5_FHzg_uLEtUQA==/109951167773880633.jpg",
+    altUrl:
+      "http://p1.music.126.net/oJorrgJ3IotZUAbZkBMuFw==/109951167771736533.jpg",
+  },
+  {
+    name: "李荣浩",
+    picUrl:
+      "http://p1.music.126.net/mkWDxcXZk8W3NwdCEMg00A==/109951165693668650.jpg",
+    altUrl:
+      "http://p1.music.126.net/MST3f1Uv77SwFyTf0Gcj5w==/109951168324499292.jpg",
+  },
+  {
+    name: "告五人",
+    picUrl:
+      "http://p1.music.126.net/Xyoa72EqbaHGHiSfL5D_qA==/109951168306629780.jpg",
+    altUrl:
+      "http://p1.music.126.net/MbhzNEtk-c3KybdGtq3ueQ==/109951168306621485.jpg",
+  },
+  {
+    name: "周杰伦",
+    picUrl:
+      "http://p1.music.126.net/BbR3TuhPULMLDV0MjczI4g==/109951165793869641.jpg",
+    altUrl:
+      "http://p1.music.126.net/Esjm32Q05PQoX8pF008u7w==/109951165793871057.jpg",
+  },
+  {
+    name: "毛不易",
+    picUrl:
+      "http://p1.music.126.net/W5LXS0-I-P9Wk6lQpS6HGA==/109951165787072463.jpg",
+    altUrl:
+      "http://p1.music.126.net/3L4q_THtqVP0Tuofry303Q==/109951168543731054.jpg",
+  },
+  {
+    name: "张杰",
+    picUrl:
+      "http://p1.music.126.net/-Of37SoslyjWsPhLKTogqQ==/109951168167006631.jpg",
+    altUrl:
+      "http://p1.music.126.net/z2HqHy_CXghF0ehV-CzL9g==/109951168234197887.jpg",
+  },
+  {
+    name: "许嵩",
+    picUrl:
+      "http://p1.music.126.net/_D9P0JKRDYm3jEay9EfhRw==/109951163536274581.jpg",
+    altUrl:
+      "http://p1.music.126.net/ATZ8-mOxophKXrLC0iXMZw==/109951163536269820.jpg",
+  },
+];
