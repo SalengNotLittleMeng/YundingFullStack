fbb166b7b8dce4508c9372169968e3087a7a4a0c
薛江槟 <1753456830@qq.com>
2023-04-21 17:21:03 +0800

    全栈-薛江槟-Vue第三次任务

---
+++ src/七期学员文件夹/薛江槟/全栈-薛江槟-Vue第三次任务/src/data/hotSong.js
@@ -0,0 +1,61 @@
+export default {
+  name: "热歌榜",
+  coverImgUrl:
+    "https://p1.music.126.net/ZyUjc7K_GDpD8MO1-GQkmA==/109951166952706664.jpg",
+
+  description:
+    "云音乐热歌榜：云音乐用户一周内收听所有线上歌曲官方TOP排行榜，每日更新。",
+  
+  tracks: [
+    {
+      name: "雪 Distance",
+      picUrl:
+        "http://p3.music.126.net/cGTKkFl5qcXIDYPo7PrmDA==/109951168431655039.jpg",
+    },
+    {
+      name: "我记得",
+      picUrl:
+        "http://p4.music.126.net/FCWD6ibS2JK2B3QAnXuzwQ==/109951167805892385.jpg",
+    },
+    {
+      name: "把回忆拼好给你",
+      picUrl:
+        "http://p4.music.126.net/CBx2K_jEN3SNWwYztagPPw==/109951164485969446.jpg",
+    },
+    {
+      name: "精卫",
+      picUrl:
+        "http://p3.music.126.net/jSH_ikeooxveWl0BTc3Xkg==/109951166786983190.jpg",
+    },
+    {
+      name: "姑娘在远方",
+      picUrl:
+        "http://p4.music.126.net/69MfQfl-rA0IwEuRSCEyNw==/109951168307420773.jpg",
+    },
+    {
+      name: "我的美丽feat.海洋Bo",
+      picUrl:
+        "http://p4.music.126.net/8ZpCzo4gEeCAQyXAGYXKUQ==/109951167854450663.jpg",
+    },
+    {
+      name: "可能",
+      picUrl:
+        "http://p3.music.126.net/Lk26KqRDPVbbhB4rQRGnPw==/109951167570919875.jpg",
+    },
+    {
+      name: "在你的身边",
+      picUrl:
+        "http://p3.music.126.net/AYNBdRxJ8EdZo4xFjp7b4Q==/109951163191178425.jpg",
+    },
+    {
+      name: "若把你",
+      picUrl:
+        "http://p3.music.126.net/M877M2-VhWZiLPVFORf9iQ==/109951163401482434.jpg",
+    },
+    {
+      name: "唯一",
+      picUrl:
+        "http://p4.music.126.net/5URIeP6GjMFg_hKhGloNTA==/109951165585701063.jpg",
+    },
+  ],
+};
