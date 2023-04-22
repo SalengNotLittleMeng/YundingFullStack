d1f4d0ba418e39a40d43ad4843625ce0e11eaeef
aaaaeeeen <3313416266@qq.com>
2023-04-22 19:13:51 +0800

    全栈-李静怡-vue第三次学习任务

---
+++ src/七期学员文件夹/李静怡/全栈-李静怡-vue第二次学习任务/src/components/AddItem.vue
@@ -0,0 +1,24 @@
+<template>
+    
+        <td><input type="text" v-model="newbook.name"></td>
+        <td><input type="text" v-model="newbook.price"></td>
+        <td>
+            <button @click="decrease" :disabled="this.newcount<=0">-</button>
+            {{ newcount }}
+            <button @click="increase">+</button>
+        </td>
+        
+    
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
