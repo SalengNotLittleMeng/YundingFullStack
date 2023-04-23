976385c78340ea84fbe6e714d7e7995350202d37
贾京浩 <3423891463@qq.com>
2023-04-23 21:49:18 +0800

    全栈-贾京浩-Vue第三次任务

---
+++ src/七期学员文件夹/贾京浩/全栈-贾京浩-Vue第三次任务/src/components/ListHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="listheader">
+        <div class="header_left">
+            <h2><slot name="ListName"></slot></h2>
+            <p><slot name="ListInfo"></slot></p>
+        </div>
+        <slot name="ListPic"></slot>
+    </div>
+    <hr/>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.listheader {
+    display: flex;
+    justify-content: space-between;
+}
+
+.header_left {
+    margin-right: 35px;
+}
+.header {
+    display: flex;
+    justify-content: space-between;
+}
+
+hr {
+    border-width: 2px;
+    border-color: black;
+}
+</style>
