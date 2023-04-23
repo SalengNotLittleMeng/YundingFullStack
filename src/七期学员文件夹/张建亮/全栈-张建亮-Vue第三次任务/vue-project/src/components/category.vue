3f131a15e6846fe7a67cf750d52e7a2ef55a6768
liang <2067750768@qq.com>
2023-04-23 16:54:48 +0800

    全栈-张建亮-Vue第三次任务

---
+++ src/七期学员文件夹/张建亮/全栈-张建亮-Vue第三次任务/vue-project/src/components/category.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="category">
+        <h3>{{ title }}</h3>
+        <!-- 使用默认插槽 -->
+        <slot></slot>
+        <!-- 使用具名插槽 -->
+        <slot name="description"></slot>
+        <slot name="list"></slot>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Category',
+    props: ['title'],
+
+
+}
+</script>
+
+
+<style scoped>
+.category {
+    margin: 0 auto;
+    text-align: left;
+    background: linear-gradient(315deg, #42d392 25%, #647eff);
+    opacity: 0.8;
+
+}
+
+h3 {
+    text-align: center;
+    background-color: skyblue;
+    opacity: 0.8;
+}
+</style>
