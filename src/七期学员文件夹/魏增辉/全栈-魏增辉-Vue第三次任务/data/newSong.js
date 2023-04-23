0b01f24257945b8218c042a3ca254a5616d75bb6
hasenberger1111 <113869187+hasenberger1111@users.noreply.github.com>
2023-04-23 21:53:05 +0800

    全栈-魏增辉-Vue第三次任务

---
+++ src/七期学员文件夹/魏增辉/全栈-魏增辉-Vue第三次任务/data/newSong.js
@@ -0,0 +1,60 @@
+export default {
+  name: "新歌榜",
+  coverImgUrl:
+    "https://p1.music.126.net/wVmyNS6b_0Nn-y6AX8UbpQ==/109951166952686384.jpg",
+  description:
+    "云音乐新歌榜：云音乐用户一周内收听所有新歌（一月内最新发行） 官方TOP排行榜，每天更新。",
+
+  tracks: [
+    {
+      name: "アイドル",
+      picUrl:
+        "http://p3.music.126.net/4EQzPt4OaZraiSCRGpINwQ==/109951168506561762.jpg",
+    },
+    {
+      name: "声音",
+      picUrl:
+        "http://p3.music.126.net/eble6u8GXivcHyZ9PS6P0w==/109951168475709963.jpg",
+    },
+    {
+      name: "身骑白马",
+      picUrl:
+        "http://p3.music.126.net/iEdL_IAsaIRwNX10HtNsGg==/109951168540070885.jpg",
+    },
+    {
+      name: "明天",
+      picUrl:
+        "http://p3.music.126.net/rYPlv-TJno4hbxEMT7se1Q==/109951168520849181.jpg",
+    },
+    {
+      name: "Slow",
+      picUrl:
+        "http://p4.music.126.net/pC6aJUZMTRe_rD_6t_EbYw==/109951168548837659.jpg",
+    },
+    {
+      name: "雨过天不晴",
+      picUrl:
+        "http://p3.music.126.net/5bqKKSxhI2w-LCicrqZPvg==/109951168483915678.jpg",
+    },
+    {
+      name: "芯房",
+      picUrl:
+        "http://p3.music.126.net/E1RzJTmi-qCYAUQ9a1khhw==/109951168536334922.jpg",
+    },
+    {
+      name: "床",
+      picUrl:
+        "http://p3.music.126.net/I7iZevCpkwfCj75KrNUkLw==/109951168511501593.jpg",
+    },
+    {
+      name: "冠军情歌",
+      picUrl:
+        "http://p4.music.126.net/rYPlv-TJno4hbxEMT7se1Q==/109951168520849181.jpg",
+    },
+    {
+      name: "迷宫",
+      picUrl:
+        "http://p3.music.126.net/KA4OfYKYT1b_3W94CIrwDg==/109951168500881027.jpg",
+    },
+  ],
+};
---
+++ src/七期学员文件夹/魏增辉/全栈-魏增辉-Vue第三次任务/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+   <div class="Title">
+    <h1 class="title">热门歌手</h1>
+   </div>
+   <Box1 :DATAS = "artist"></Box1>
+   
+   <div>
+    <h1>榜单</h1>
+   </div>
+   <div class="Ending">
+   <Box2 :DATAS = "hotSong"></Box2>
+   <Box2 :DATAS = "newSong"></Box2>
+   <Box2 :DATAS = "originSong"></Box2>
+   </div>
+</template>
+
+<script>
+import artist from "../data/artist.js"
+import hotSong from "../data/hotSong.js"
+import newSong from "../data/newSong.js"
+import originSong from "../data/originSong.js"
+import Box1 from "./components/Box1.vue"
+import Box2 from "./components/Box2.vue"
+export default {
+  data(){
+    return{
+      artist,
+      hotSong,
+      newSong,
+      originSong,
+    }
+  },
+
+  components: {
+    Box1,
+    Box2,
+  }
+}
+</script>
+
+<style scoped>
+      .Ending{
+         margin: 0 auto;
+         display: flex;
+         justify-content: space-around;
+         width: 1500px;
+         border-style: solid;
+        border-top-width: 5px;
+        border-right-width: 0px;
+        border-bottom-width: 5px;
+        border-left-width: 0px;
+      }
+      h1{
+        position: relative;
+        left: 450px;
+      }
+
+</style>
