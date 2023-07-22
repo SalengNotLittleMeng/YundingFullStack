5a7f131245b8e72520abd0f11683332bf693ef8b
marongzhen <2712268578@qq.com>
2023-07-22 13:57:37 +0800

    全栈-马荣臻-Vue第五次任务

---
+++ src/七期学员文件夹/马荣臻/全栈-马荣臻-Vue第五次任务/src/Views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="textarea">
+    <h2 class="title">我是Home组件</h2>
+    <div class="home-nav">
+      <!-- 路由导航 -->
+      <router-link to="/home/list">展示列表</router-link>
+      <p>&nbsp; | &nbsp;</p>
+      <router-link to="/home/selected">你的选择</router-link>
+    </div>
+    <!-- 设置组件缓存，防止组件卸载导致数据清除 -->
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </div>
+</template>
+
+<script setup>
+//声明需要缓存的组件
+import { ref } from "vue";
+const Component = ref("HomeList", "HomeSelected");
+
+</script>
+
+<style scoped>
+.home-nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  text-align: center;
+  color: rgb(66, 184, 131);
+}
+.router-link-active {
+  display: inline;
+  color: rgb(66, 184, 131);
+}
+</style>
---
+++ src/七期学员文件夹/马荣臻/全栈-马荣臻-Vue第五次任务/src/Views/HomeSelected.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="textarea">
+    <p>{{ selectedEle }}</p>
+  </div>
+</template> 
+
+<script setup>
+import { ref, onMounted, onActivated, onUnmounted } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const selectedEle = ref("暂无选择");
+
+//组件缓存，设置onActivited钩子更新数据
+onActivated(() => {
+  if (route.query.selectedEle) {
+    selectedEle.value = JSON.parse(route.query.selectedEle);
+  }
+});
+</script>
+
+<style scoped>
+.textarea {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
