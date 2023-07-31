c98c8887015bba1d017f47dd8a26730799e1312c
aaaaeeeen <3313416266@qq.com>
2023-07-31 10:14:29 +0800

    全栈-李静怡-vue第五次学习任务

---
+++ src/七期学员文件夹/李静怡/全栈-李静怡-vue第五次学习任务/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <h3>app</h3>
+    <div class="nav">
+      <router-link to="/home">首页</router-link>
+      |
+      <router-link to="/about">关于</router-link>
+    </div>
+    <!-- <div class="rr">
+    <router-view></router-view>
+    </div> -->
+    
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+  .app{
+    height: 100px;
+    width: 50%;
+    margin: 0 auto;
+
+  }
+</style>
---
+++ src/七期学员文件夹/李静怡/全栈-李静怡-vue第五次学习任务/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/index'
+
+// const app = createApp(App)
+// app.use(router)
+// app.mount('#app')
+// app.use(router)
+createApp(App).use(router).mount('#app')
+
